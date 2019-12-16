71119625705042520987a6e48424b7f272d7daa8
Martin Loesch <martin@martinloesch.net>
2019-12-16 01:04:04 +0100

    Added structure and input for day 13 in Kotlin.

---
+++ Kotlin/days/src/day13.kt
@@ -0,0 +1,34 @@
+import dukemarty.aoc2019.common.IntcodeInterpreter
+import dukemarty.aoc2019.common.IntcodeProgram
+import java.io.BufferedReader
+import java.io.FileReader
+
+fun main(args: Array<String>) {
+    println("--- Day 13: Care Package ---");
+
+    val br = BufferedReader(FileReader("puzzle_input/day13-input1.txt"))
+    var line = br.readLine()
+
+//    line = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
+//    line = "1102,34915192,34915192,7,4,7,99,0"
+//    line = "104,1125899906842624,99"
+
+    day13PartOne(line)
+
+//    day7partTwo(line)
+}
+
+
+fun day13PartOne(line: String) {
+    println("\n--- Part One ---")
+
+    val program = IntcodeProgram(line)
+
+    val interpreter = IntcodeInterpreter(program)
+
+    interpreter.runProgram()
+
+    println("Output of program: ${interpreter.outputBuffer}")
+    println("Simpler: ${interpreter.outputBuffer.joinToString { it.value.toString() }}")
+}
+
